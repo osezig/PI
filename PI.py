74919e01fdc87ada6e9256e37488452aa7fc9139
Sergey_Iagfarov <113824525+osezig@users.noreply.github.com>
2024-11-26 16:38:44 +0500

    Add files via upload

---
+++ PI.py
@@ -0,0 +1,30 @@
+calls = 0
+
+
+def count_calls():
+    global calls
+    calls = calls + 1
+
+
+def string_info(string):
+    count_calls()
+    return (len(string), string.upper(), string.lower())
+
+def is_contains(string, list_to_search):
+    count_calls()
+    for item in list_to_search:
+        if string in item:
+            return string.lower()
+        print(True)
+    return False
+
+
+print(string_info('Capybara'))
+
+print(string_info('Armageddon'))
+
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
+
+print(is_contains('cycle', ['recycling', 'cyclic']))
+
+print(calls)
